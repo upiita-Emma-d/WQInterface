0b6c3031dcadadcfe9239f64d808abd3511a31df
AD <diaz.ortiz.axel.emmanuel@gmail.com>
2023-03-15 13:26:52 -0600

    add in gitignore venv

--- client/interface_helpers/interfacecustom.py
+++
@@ -1,4 +0,0 @@
-
-def add_port(self):
-    self.com_port= str(self.cb_port.currentText()) 
-    print(self.com_port)
--- client/prueba_termica.py
+++
@@ -1,5 +0,0 @@
-from w1thermsensor import W1ThermSensor, Sensor
-
-sensor = W1ThermSensor(Sensor.DS18B20, "0517c1fd10ff")
-tem_in_c = sensor.get_temperature()
-print(tem_in_c)
